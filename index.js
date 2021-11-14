82e75527f98dd58bc643b9bc991b49aa321d6927
Kenneth Mitchell <kmitchel46725@gmail.com>
2021-11-14 18:12:29 -0500

    Move db from mongoinsert into index.

--- index.js
+++ index.js
@@ -240,6 +240,10 @@ client.on("connect", function() {
     client.subscribe("#")
 })
 
+let leveldb = require("level")("./hemdb", {
+    valueEncoding: "json"
+})
+
 client.on("message", function(topic, message) {
     switch (topic) {
         case "power/W":
@@ -271,31 +275,26 @@ client.on("message", function(topic, message) {
             child.stdin.write("update " + __dirname + "/hem-out.rrd N:" +
                 dataOut + "\n")
             break
-
         case "temp/2809853f030000a7":
             var dataUpper = Number(message.toString())
             child.stdin.write("update " + __dirname + "/hem-upper.rrd N:" +
                 dataUpper + "\n")
             break
-
         case "temp/2813513f03000072":
             var dataLower = Number(message.toString())
             child.stdin.write("update " + __dirname + "/hem-lower.rrd N:" +
                 dataLower + "\n")
             break
-
         case "temp/289fa756b5013c68":
             var dataACHigh = Number(message.toString())
             child.stdin.write("update " + __dirname + "/hem-achigh.rrd N:" +
                 dataACHigh + "\n")
             break
-
         case "temp/2874913c46200105":
             var dataACLow = Number(message.toString())
             child.stdin.write("update " + __dirname + "/hem-aclow.rrd N:" +
                 dataACLow + "\n")
             break
-
         case "hvac/state":
             if (message.toString() == "Heating" || message.toString() == "HeatOn") {
                 child.stdin.write("update " + __dirname + "/hem-heat.rrd N:100\n")
@@ -309,4 +308,140 @@ client.on("message", function(topic, message) {
             }
             break
     }
+
+
+    topic = topic.split("/").join("-");
+
+    if (topic.indexOf("hvac-state") > -1) {
+        if (message.indexOf("CoolOn") > -1 || message.indexOf("Cooling") > -1) {
+            updateCnt(leveldb, "hvac-cool", 0.25);
+            let key = "hvac-cool-28-" + getMonthBucket();
+            leveldb.get(key, function(error, data) {
+                if (!error || typeof data.d === 'number') {
+                    client.publish("hvac/coolTime", data.d.toFixed(2));
+                }
+            })
+        } else if (message.indexOf("HeatOn") > -1 || message.indexOf("Heating") > -1) {
+            updateCnt(leveldb, "hvac-heat", 0.25);
+            let key = "hvac-heat-28-" + getMonthBucket();
+            leveldb.get(key, function(error, data) {
+                if (!error || typeof data.d === 'number') {
+                    client.publish("hvac/heatTime", data.d.toFixed(2));
+                }
+            })
+        }
+    } else if (topic.indexOf("power-W") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+        updateCnt(leveldb, "power-kWh", 0.001);
+        let key = "power-kWh-28-" + getMonthBucket();
+        leveldb.get(key, function(error, data) {
+            if (!error || typeof data.d === 'number') {
+                client.publish("power/kWh", data.d.toFixed(3));
+            }
+        })
+    } else if (topic.indexOf("water-GPM") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+        updateCnt(leveldb, "water-Gal", 0.25)
+        let key = "water-Gal-28-" + getMonthBucket();
+        leveldb.get(key, function(error, data) {
+            if (!error || typeof data.d === 'number') {
+                client.publish("water/Gal", data.d.toFixed(2))
+            }
+        })
+    } else if (topic.indexOf("temp-") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+    }
+
+
 })
+
+
+function getMonthBucket() {
+    let month;
+    if (new Date().getDate() > 6) {
+        month = new Date().getMonth();
+    } else {
+        month = new Date().getMonth() - 1;
+    }
+    return new Date(new Date().getFullYear(), month, 7).getTime();
+}
+
+function insertNow(db, topic, message) {
+    let key = topic;
+
+    db.get(key, function(error, data) {
+        if (error) {
+            db.put(key, [{
+                "t": Date.now(),
+                "d": Number(message)
+            }])
+        } else {
+            let last = data.push({
+                "t": Date.now(),
+                "d": Number(message)
+            })
+            if (((data[last - 1].t - data[0].t) / 1000 / 60 / 60) > 48) {
+                data.shift()
+            }
+            db.put(key, data)
+        }
+    })
+}
+
+function insertAvg(db, topic, message) {
+    let buckets = [
+        [5, Math.floor(Date.now() / (5 * 60 * 1000)) * 5 * 60 * 1000],
+        [15, Math.floor(Date.now() / (15 * 60 * 1000)) * 15 * 60 * 1000],
+        [60, Math.floor(Date.now() / (60 * 60 * 1000)) * 60 * 60 * 1000],
+        [24, new Date().setHours(0, 0, 0, 0)],
+        [28, getMonthBucket()]
+    ];
+
+    buckets.forEach(function(currentValue) {
+        let key = topic + "-" + currentValue[0] + "-" + currentValue[1];
+        db.get(key, function(error, data) {
+            if (error) {
+                db.put(key, {
+                    "cnt": 1,
+                    "acc": Number(message),
+                    "min": Number(message),
+                    "max": Number(message)
+                })
+            } else {
+                data.cnt += 1
+                data.acc += Number(message)
+                data.min = Math.min(data.min, Number(message))
+                data.max = Math.max(data.max, Number(message))
+                db.put(key, data)
+            }
+        })
+    })
+}
+
+function updateCnt(db, topic, incValue) {
+    let buckets = [
+        [5, Math.floor(Date.now() / (5 * 60 * 1000)) * 5 * 60 * 1000],
+        [15, Math.floor(Date.now() / (15 * 60 * 1000)) * 15 * 60 * 1000],
+        [60, Math.floor(Date.now() / (60 * 60 * 1000)) * 60 * 60 * 1000],
+        [24, new Date().setHours(0, 0, 0, 0)],
+        [28, getMonthBucket()]
+    ];
+
+    buckets.forEach(function(currentValue) {
+        let key = topic + "-" + currentValue[0] + "-" + currentValue[1];
+        db.get(key, function(error, data) {
+            if (error) {
+                db.put(key, {
+                    "d": incValue
+                })
+            } else {
+                db.put(key, {
+                    "d": Number(Number(data.d + incValue).toFixed(3))
+                })
+            }
+        })
+    })
+}
--- mongoinsert.js
+++ mongoinsert.js
@@ -1,73 +1,61 @@
-let mqtt = require('mqtt');
-let client = mqtt.connect('mqtt://localhost', {
-    clientId: 'mongoinsert' + Math.random().toString(16).substr(2, 8)
+let mqtt = require("mqtt");
+let client = mqtt.connect("mqtt://localhost", {
+    clientId: "dbInsert" + Math.random().toString(16).substr(2, 8)
 });
 
-client.on('connect', function() {
-    client.subscribe('#');
+client.on("connect", function() {
+    client.subscribe("#");
 });
 
-let mongodb = require('mongodb');
-let MongoClient = mongodb.MongoClient;
-let url = 'mongodb://localhost:27017/hem';
+let leveldb= require("level")("./hemdb", {valueEncoding: "json"})
 
-let leveldb = require('level')('./hemdb', {
-    valueEncoding: 'json'
-})
+client.on("message", function(topic, message) {
+    topic = topic.split("/").join("-");
 
-MongoClient.connect(url, function(err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    }
-
-    client.on('message', function(topic, message) {
-        topic = topic.split('/').join('-');
-
-        if (topic.indexOf('hvac-state') > -1) {
-            if (message.indexOf('CoolOn') > -1 || message.indexOf('Cooling') > -1) {
-                updateCnt(leveldb, 'hvac-cool', 0.25);
-            } else if (message.indexOf('HeatOn') > -1 || message.indexOf('Heating') > -1) {
-                updateCnt(leveldb, 'hvac-heat', 0.25);
-
-                db.collection('hvac-heat-28')
-                    .find({
-                        t: getMonthBucket()
-                    })
-                    .toArray(function(err, result) {
-                        if (result.length === 1) {
-                            client.publish('hvac/heatTime', result[0].d.toFixed(3));
-                        }
-                    });
-            }
-        } else if (topic.indexOf('power-W') > -1) {
-            insertNow(leveldb, topic, message)
-            insertAvg(leveldb, topic, message)
-            updateCnt(leveldb, 'power-kWh', 0.001);
-            let key = "power-kWh-28-" + getMonthBucket();
+    if (topic.indexOf("hvac-state") > -1) {
+        if (message.indexOf("CoolOn") > -1 || message.indexOf("Cooling") > -1) {
+            updateCnt(leveldb, "hvac-cool", 0.25);
+            let key = "hvac-cool-28-" + getMonthBucket();
             leveldb.get(key, function(error, data) {
-                if (!error) {
-                    client.publish('power/kWh', data.d.toFixed(3));
+                if (!error || typeof data.d === 'number') {
+                    client.publish("hvac/coolTime", data.d.toFixed(2));
                 }
             })
-        } else if (topic.indexOf('water-GPM') > -1) {
-            insertNow(leveldb, topic, message)
-            insertAvg(leveldb, topic, message)
-            updateCnt(leveldb, 'water-Gal', 0.25)
-            db.collection('water-Gal-28')
-                .find({
-                    t: getMonthBucket()
-                })
-                .toArray(function(err, result) {
-                    if (result.length === 1) {
-                        client.publish('water/Gal', result[0].d.toFixed(3));
-                    }
-                });
-        } else if (topic.indexOf('temp-') > -1) {
-            insertNow(leveldb, topic, message)
-            insertAvg(leveldb, topic, message)
+        } else if (message.indexOf("HeatOn") > -1 || message.indexOf("Heating") > -1) {
+            updateCnt(leveldb, "hvac-heat", 0.25);
+            let key = "hvac-heat-28-" + getMonthBucket();
+            leveldb.get(key, function(error, data) {
+                if (!error || typeof data.d === 'number') {
+                    client.publish("hvac/heatTime", data.d.toFixed(2));
+                }
+            })
         }
-    });
-});
+    } else if (topic.indexOf("power-W") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+        updateCnt(leveldb, "power-kWh", 0.001);
+        let key = "power-kWh-28-" + getMonthBucket();
+        leveldb.get(key, function(error, data) {
+            if (!error || typeof data.d === 'number') {
+                client.publish("power/kWh", data.d.toFixed(3));
+            }
+        })
+    } else if (topic.indexOf("water-GPM") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+        updateCnt(leveldb, "water-Gal", 0.25)
+        let key = "water-Gal-28-" + getMonthBucket();
+        leveldb.get(key, function(error, data) {
+            if (!error || typeof data.d === 'number') {
+                client.publish("water/Gal", data.d.toFixed(2))
+            }
+        })
+    } else if (topic.indexOf("temp-") > -1) {
+        insertNow(leveldb, topic, message)
+        insertAvg(leveldb, topic, message)
+    }
+})
+
 
 function getMonthBucket() {
     let month;
@@ -93,7 +81,7 @@ function insertNow(db, topic, message) {
                 "t": Date.now(),
                 "d": Number(message)
             })
-            if (((data[last - 1].t - data[0].t) / 1000 / 60 / 60) > 48) data.shift()
+            if (((data[last - 1].t - data[0].t) / 1000 / 60 / 60) > 48) {data.shift()}
             db.put(key, data)
         }
     })
@@ -109,7 +97,7 @@ function insertAvg(db, topic, message) {
     ];
 
     buckets.forEach(function(currentValue) {
-        let key = topic + '-' + currentValue[0] + '-' + currentValue[1];
+        let key = topic + "-" + currentValue[0] + "-" + currentValue[1];
         db.get(key, function(error, data) {
             if (error) {
                 db.put(key, {
@@ -139,19 +127,17 @@ function updateCnt(db, topic, incValue) {
     ];
 
     buckets.forEach(function(currentValue) {
-        let key = topic + '-' + currentValue[0] + '-' + currentValue[1];
+        let key = topic + "-" + currentValue[0] + "-" + currentValue[1];
         db.get(key, function(error, data) {
             if (error) {
                 db.put(key, {
-                    "d": 0.001
+                    "d": incValue
                 })
             } else {
                 db.put(key, {
                     "d": Number(Number(data.d + incValue).toFixed(3))
                 })
-                //console.log(key + " " + JSON.stringify(data))
             }
         })
     })
-
 }
